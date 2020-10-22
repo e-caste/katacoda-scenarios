8b44eb0bbf664d6a4ae1e3ad5eff66ba8fcfa38e
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-22 18:11:28 +0200

    call set-up-nginx.sh, improve readability

--- weee-open-sardina/set-up-repo.sh
+++ weee-open-sardina/set-up-repo.sh
@@ -1,1 +1,8 @@
-git clone https://github.com/weee-open/sardina && cd sardina && pip install -r requirements.txt && apt install -y cloc && echo && echo "You can now run python3 main.py or click in the left panel!"
+git clone https://github.com/weee-open/sardina
+cd sardina
+pip install -r requirements.txt
+apt update
+apt install -y cloc 
+bash set-up-nginx.sh
+echo "
+You can now run python3 main.py or click in the left panel!"
