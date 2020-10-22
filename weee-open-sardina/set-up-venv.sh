7ff1e5b1aad919306059b04b60abf4caaac78760
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-22 15:21:38 +0200

    fix: apt installs new packages automatically

--- weee-open-sardina/set-up-venv.sh
+++ weee-open-sardina/set-up-venv.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/weee-open/sardina
 cd sardina
-apt install python3-venv
+apt install -y python3 python3-venv python3-pip
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
