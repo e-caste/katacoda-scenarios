cfd423b5f2f3674d494c3297bc111bc279d4362a
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-22 18:25:20 +0200

    move set-up-nginx commands into set-up-repo

--- weee-open-sardina/set-up-repo.sh
+++ weee-open-sardina/set-up-repo.sh
@@ -1,8 +1,22 @@
 git clone https://github.com/weee-open/sardina
 cd sardina
 pip install -r requirements.txt
+
 apt update
-apt install -y cloc 
-bash set-up-nginx.sh
+apt install -y cloc nginx
+
+chmod -R 755 /root
+echo "server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+  root /root/sardina/output;
+  server_name _;
+  location / {
+	autoindex on;
+	try_files $uri $uri/ =404;
+  }
+}" > /etc/nginx/sites-available/default
+systemctl restart nginx
+
 echo "
 You can now run python3 main.py or click in the left panel!"
