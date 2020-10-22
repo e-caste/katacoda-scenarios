86710e21e27daecd9da005e789b3d30bda2abf0b
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-22 16:26:27 +0200

    add final message

--- weee-open-sardina/set-up-repo.sh
+++ weee-open-sardina/set-up-repo.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/weee-open/sardina && cd sardina && pip install -r requirements.txt && apt install -y cloc
+git clone https://github.com/weee-open/sardina && cd sardina && pip install -r requirements.txt && apt install -y cloc && echo && echo "You can now run python3 main.py or click in the left panel!"
