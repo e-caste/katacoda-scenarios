1e6b801a07d3d367987c12a32f15a0a91db34b86
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 09:34:38 +0200

    install sqlite3

--- weee-open-weeehire-ng/set-up-repo.sh
+++ weee-open-weeehire-ng/set-up-repo.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/weee-open/weeehire-ng
 cd weeehire-ng
+apt update
+apt install -y sqlite3 < /dev/null
 ./launch.sh
 
 clear && echo "You should see a development version of the WEEEhire in the top panel!"
